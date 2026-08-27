936700b71e22b36903ba5b2507ea8ba5b3bc56b5
Saan-Irl <siamexists88@gmail.com>
2026-08-27 08:22:45 +0600

    Update owner contact ID in welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -41,7 +41,7 @@ module.exports = {
 				const ownerContactId =
 					(global.GoatBot && global.GoatBot.config && global.GoatBot.config.ownerID) ||
 					(global.config && (global.config.BOTOWNERID || global.config.BOTOWNER)) ||
-					"61587427123882";
+					"100075454605535";
 
 				const welcomeText = getLang("welcomeMessage", prefix);
 
